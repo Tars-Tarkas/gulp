3378c1a1c783edb4f6b97321eecceb61f0b03c81
Andrey Molodyk <87598011+AndrewMolodyk@users.noreply.github.com>
2021-10-10 21:48:30 +0700

    6

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 const fileinclude = require('gulp-file-include');
 
-let project_folder = "dist";
-let source_folder = "#src";
+let project_folder = "dist"; // собранный проект
+let source_folder = "#src"; // папка с исходниками
+let fs = require('fs');
 
 let path = {
     build: {
@@ -42,7 +43,11 @@ let { src, dest } = require('gulp'),
     webp = require("gulp-webp"), // конвертор в ВебП
     webphtml = require("gulp-webp-html"),
     webpcss = require("gulp-webpcss"),
-    svgSprite = require("gulp-svg-sprite"); //собирает свг файлы
+    svgSprite = require("gulp-svg-sprite"), //собирает свг файлы
+    ttf2woff = require("gulp-ttf2woff"), //конвертируем шрифты
+    ttf2woff2 = require("gulp-ttf2woff2"), //конвертируем шрифты
+    fonter = require("gulp-fonter"), //конвертируем из otf в ttf
+    googleWebFonts = require("gulp-google-webfonts");
 
 function browserSync(params) {
     browsersync.init({
@@ -124,6 +129,59 @@ function images() {
         .pipe(browsersync.stream())
 }
 
+function fonts() {
+    src(path.src.fonts)
+        .pipe(ttf2woff())
+        .pipe(dest(path.build.fonts));
+    return src(path.src.fonts)
+        .pipe(ttf2woff2())
+        .pipe(dest(path.build.fonts));
+}
+
+gulp.task('otf2ttf', function() { //ручной запуск
+    return src([source_folder + '/fonts/*.otf'])
+        .pipe(fonter({
+            formats: ['ttf']
+        }))
+        .pipe(dest(source_folder + '/fonts/'));
+})
+
+/*function fontsStyle() { // пока не работает
+    let file_content = fs.readFileSync(source_folder + '/scss/_fonts.scss');
+    if (file_content == '') {
+        fs.writeFile(source_folder + '/scss/_fonts.scss', '', callBack);
+        return fs.readdir(path.build.fonts, function(err, items) {
+            if (items) {
+                let c_font_name;
+                for (var x = 0; x < items.length; x++) {
+                    let font_name = items[x].split('.');
+                    font_name = font_name[0];
+                    if (c_font_name != font_name) {
+                        // fs.appendFile(source_folder + '/scss/_fonts.scss', '@include font("' + fontname + '")', function() {})
+                        fs.appendFile(source_folder + '/scss/_fonts.scss', `@include font("${font_name}", "${font_name}", "400", "normal");\r\n`, () => {});
+                    }
+                    c_font_name = font_name;
+                }
+            }
+        })
+    }
+}
+
+function callBack() {
+
+}*/
+var options = {};
+
+gulp.task('fontsG', function() { //качаем шрифты с Гугла
+    return src(source_folder + '/fonts/fonts.list')
+        .pipe(googleWebFonts(options))
+        .pipe(dest(path.build.fonts));
+});
+
+
+
+
+
 function watchFiles() { //прослушка файлов на изменение и с последующим обновлением
     gulp.watch([path.watch.html], html);
     gulp.watch([path.watch.css], css);
@@ -135,7 +193,7 @@ function clean() {
     return del(path.clean);
 }
 
-let build = gulp.series(clean, gulp.parallel(css, html, js, images));
+let build = gulp.series(clean, gulp.parallel(css, html, js, images, fonts));
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
 
@@ -143,6 +201,8 @@ exports.html = html;
 exports.css = css;
 exports.js = js;
 exports.images = images;
+exports.fonts = fonts;
+//exports.fontsStyle = fontsStyle;
 exports.build = build;
 exports.watch = watch;
 exports.default = watch;
